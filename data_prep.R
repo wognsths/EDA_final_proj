add02faa10a4392b8b59b79e1c7ca8cdcd8cf76f
wognsths <2021122006@yonsei.ac.kr>
2024-11-28 13:38:10 +0900

    ..

---
+++ data_prep.R
@@ -0,0 +1,88 @@
+## This file is for data analysis & data loaders
+library(tidyverse);library(ggplot2);library(sf);library(lubridate);library(data.table)
+
+Crime_Data <- read_csv("Crime_Data_from_2020_to_Present.csv")
+Police_station <- read_csv("~/EDA_final_proj/Sheriff_and_Police_Stations.csv")
+boundary <- st_read("LAPD_Div/LAPD_Divisions.shp")
+boundary <- st_transform(boundary, crs = 4326)
+
+CD <- Crime_Data %>%
+  mutate(
+    `DATE OCC` = as.Date(`DATE OCC`, format = "%m/%d/%Y"),
+    `Date Rptd` = as.Date(`Date Rptd`, format = "%m/%d/%Y"),
+    OCC_year = year(`DATE OCC`),
+    `Dur Rptd` = as.numeric(`Date Rptd` - `DATE OCC`),
+    Severity = factor(`Part 1-2`, levels = c(1, 2), labels = c("Severe", "Less Severe")),
+    weapon_usage = ifelse(is.na(`Weapon Used Cd`), "No", "Yes"),
+    crime_status = ifelse(`Status Desc` %like% "Juv", "Juvenile", 
+                          ifelse(`Status Desc` %like% "Adult", "Adult", NA)),
+    vict_sex = ifelse(((`Vict Sex` == "-") | (`Vict Sex` == "X") | (`Vict Sex` == "")), NA, `Vict Sex`)
+  ) %>% filter(OCC_year == 2023)
+
+### DATALOADER ###
+GeoData_Loader <- function() {
+  CD %>% filter(LAT != 0, LON != 0, `Dur Rptd` > 0) -> Geo.CD
+  return(Geo.CD)
+}
+
+MosaicData_Loader <- function() {
+  CD %>% 
+    select(., c(Severity, weapon_usage, crime_status, vict_sex, `AREA NAME`)) -> Mosaic.CD
+  return(Mosaic.CD)
+}
+
+
+### Other Functions for Data Analysis ###
+
+Distance_Calculator <- function(Criteria_point, Data, option = 2) {
+  Data <- as.matrix(Data)
+  Criteria_point <- as.matrix(Criteria_point)
+  
+  # Scaling factors
+  
+  Point_for_real_dist <- Data %>% apply(., 2, range) %>% apply(.,2,mean)
+  
+  lat_scale <- 111.32  # km per degree latitude
+  lon_scale <- 111.32 * cos(Point_for_real_dist[2] * pi / 180)  # km per degree longitude
+  
+  # Apply scaling to Data and Criteria_point
+  Data_scaled <- Data
+  Data_scaled[, 1] <- Data[, 1] * lat_scale     # Scale latitude
+  Data_scaled[, 2] <- Data[, 2] * lon_scale     # Scale longitude
+  
+  Criteria_scaled <- Criteria_point
+  Criteria_scaled[, 1] <- Criteria_point[, 1] * lat_scale
+  Criteria_scaled[, 2] <- Criteria_point[, 2] * lon_scale
+  
+  n <- nrow(Data_scaled)
+  m <- nrow(Criteria_scaled)
+  
+  if (option == 2) {
+    # Euclidean distance
+    Data_norms <- rowSums(Data_scaled^2)
+    Criteria_norms <- rowSums(Criteria_scaled^2)
+    Cross_prod <- Data_scaled %*% t(Criteria_scaled)
+    
+    Distance_sq <- outer(Data_norms, rep(1, m)) + 
+      outer(rep(1, n), Criteria_norms) - 2 * Cross_prod
+    
+    Distance_sq[Distance_sq < 0] <- 0  # Correct for numerical errors
+    
+    Distance <- sqrt(Distance_sq)
+    return(Distance)
+    
+  } else if (option == 1) {
+    # Manhattan distance
+    p <- ncol(Data_scaled)
+    Distance <- matrix(0, n, m)
+    
+    for (k in 1:p) {
+      Distance <- Distance + abs(outer(Data_scaled[, k], Criteria_scaled[, k], "-"))
+    }
+    return(Distance)
+    
+  } else {
+    stop("Invalid option. Choose 1 for Manhattan distance or 2 for Euclidean distance.")
+  }
+}
+
